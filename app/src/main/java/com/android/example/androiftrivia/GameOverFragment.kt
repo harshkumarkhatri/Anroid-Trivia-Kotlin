5ab6f58009b769c3238f37e4662b465ccd7e34e5
harshkumarkhatri <mailharshkhatri@gmail.com>
2020-05-20 23:11:49 +0530

    Completed till the two screens of try again and completed

---
+++ app/src/main/java/com/android/example/androiftrivia/GameOverFragment.kt
@@ -0,0 +1,24 @@
+package com.android.example.androiftrivia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+
+import com.android.example.androiftrivia.databinding.FragmentGameOverBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class GameOverFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_game_over, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/android/example/androiftrivia/GameWonFragment.kt
@@ -0,0 +1,27 @@
+package com.android.example.androiftrivia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.android.example.androiftrivia.databinding.FragmentGameWonBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+
+
+
+
+class GameWonFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_game_won, container, false)
+        return binding.root
+    }
+}
